47f043cf5f0238270e8f8dc319a827a0f1cba2c5
Sumith <officiallysumit432@gmail.com>
2025-05-31 21:45:05 +0530

    Added Frontend UI for password Generator

---
+++ script.js
@@ -0,0 +1,8 @@
+function generatePassword() {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    let password = "";
+    for (let i = 0; i < 12; i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+    }
+    document.getElementById("generatedPassword").value = password;
+}
